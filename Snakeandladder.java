dda67d61b2167af7511b1996e9e816210a48c83e
vishal sam <vinishakr9@gmail.com>
2022-01-22 18:30:49 +0530

    Added player possition

---
+++ Snakeandladder.java
@@ -0,0 +1,12 @@
+package com.snakeandladder;
+
+public class Snakeandladder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Snake and ladder game");
+		int POSITION=0;
+		System.out.println("player position is at: " + POSITION);
+	}
+
+}
