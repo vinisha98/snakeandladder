ed9e60a043567a2c29143bad386d3b383b2e40d9
vishal sam <vinishakr9@gmail.com>
2022-01-22 21:07:41 +0530

    Added check for the option logic

--- Snakeandladder.java
+++ Snakeandladder.java
@@ -1,7 +1,9 @@
 package com.snakeandladder;
 
 public class Snakeandladder {
-
+	public static final int NO_PLAY=1;
+	public static final int LADDER=2;
+	public static final int SNAKE=3;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Snake and ladder game");
@@ -12,27 +14,43 @@ public class Snakeandladder {
 			POSITION= (POSITION + Roll);
 		}
 		else if (Roll==2) {
-			System.out.println(Roll);
-			POSITION= (POSITION + Roll);
+			System.out.println("Roll value is: " + Roll);
 		}
 		else if (Roll==3) {
-			System.out.println(Roll);
-			POSITION= (POSITION + Roll);
+			System.out.println("Roll value is: " + Roll);
 		}
 		else if (Roll==4) {
-			System.out.println(Roll);
-			POSITION= (POSITION + Roll);
+			System.out.println("Roll value is: " + Roll);
 		}
 		else if (Roll==5) {
-			System.out.println(Roll);
-			POSITION= (POSITION + Roll);
+			System.out.println("Roll value is: " + Roll);
 		}
 		else {
-			System.out.println(Roll);
-			POSITION= (POSITION + Roll);
+			System.out.println("Roll value is: " + Roll);
 		}
-		System.out.println("player position is at: " + POSITION);
-
+		
+		int Check= (int) Math.floor((Math.random() * 10) %  3) + 1;
+		System.out.println("check value is: " + Check);
+		switch (Check) {
+		case NO_PLAY:
+			System.out.println("player position is at: " + POSITION);
+			break;
+		case LADDER:
+			POSITION = POSITION + Roll;
+			System.out.println("player position is at: " + POSITION);
+			break;
+		case SNAKE:
+			POSITION = POSITION - Roll;
+			if (POSITION>0) {	
+			System.out.println("player position is at: " + POSITION);
+			}
+			else {
+				System.out.println("Player falles to position : 0");
+			}
+			break;
+		default:
+			System.out.println("Roll not valid");
+		}
 	}
 
 }
