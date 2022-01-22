22860c5815fdc7441c372fa3507bae31e7c084e4
vishal sam <vinishakr9@gmail.com>
2022-01-22 18:48:24 +0530

    Added Random dice logic

--- Snakeandladder.java
+++ Snakeandladder.java
@@ -6,7 +6,33 @@ public class Snakeandladder {
 		// TODO Auto-generated method stub
 		System.out.println("Snake and ladder game");
 		int POSITION=0;
+		int Roll= (int) Math.floor((Math.random() * 10) %  6) + 1;
+		if (Roll==1) {
+			System.out.println(Roll);
+			POSITION= (POSITION + Roll);
+		}
+		else if (Roll==2) {
+			System.out.println(Roll);
+			POSITION= (POSITION + Roll);
+		}
+		else if (Roll==3) {
+			System.out.println(Roll);
+			POSITION= (POSITION + Roll);
+		}
+		else if (Roll==4) {
+			System.out.println(Roll);
+			POSITION= (POSITION + Roll);
+		}
+		else if (Roll==5) {
+			System.out.println(Roll);
+			POSITION= (POSITION + Roll);
+		}
+		else {
+			System.out.println(Roll);
+			POSITION= (POSITION + Roll);
+		}
 		System.out.println("player position is at: " + POSITION);
+
 	}
 
 }
